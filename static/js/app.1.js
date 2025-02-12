00336f6cdecd4ee3988079ff4a83b247a9254e90
Fouche du Preez <fouche@limehouse.co.za>
2025-02-12 21:24:17 +0200

    added validation

--- static/js/app.1.js
+++ static/js/app.1.js
@@ -5,3 +5,30 @@ function resetSearch() {
 
     document.querySelector('.grid-scroll').scrollTop=0;
 }
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+function validate () {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation');
+
+    // Loop over them and prevent submission
+    forms.forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+            if (!form.checkValidity()) {
+                event.preventDefault();
+                event.stopPropagation();
+            }
+
+            form.classList.add('was-validated');
+        }, false);
+    });
+
+    document.body.addEventListener('htmx:configRequest', function (event) {
+        const form = event.target.closest('form');
+        if (form && !form.checkValidity()) {
+            event.preventDefault();
+            form.classList.add('was-validated');
+        }
+
+    });
+}
